1409aafa67b37f4c6fd303f57e751a022165902a
rjtsg <rjtsg@users.noreply.github.com>
2019-08-14 17:38:14 +0200

    added comments

--- tr2.py
+++ tr2.py
@@ -18,7 +18,10 @@ DATE = list()
 df1 = pd.DataFrame(columns=['Quarter'],index=range(0,58))
 Counter = 0
 
-for i in TickerList:
+#TickerList zou dus de afkorting moeten zijn
+#TickerName zou de volledige naam moeten zijn in de lijst
+
+for i in TickerList: #makes columns for the ESP input
     df1['ESP_{}'.format(i)] = 'NaN'
 
 for i in range(0,len(list2)): #This for-loop retrieves the good Quarters
@@ -31,16 +34,16 @@ for i in range(0,len(list2)): #This for-loop retrieves the good Quarters
     except:
         pass
     
-for i in range(0,len(TickerList)):
+for i in range(0,len(TickerList)): #Fills in the EPS column for each company
     x = TickerList[i]
     y = TickerName[i]
     url1 = 'https://www.macrotrends.net/stocks/charts/{}/{}/eps-earnings-per-share-diluted'.format(x,y)
-    res = requests.get(url)
+    res = requests.get(url1)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text)
     list2 = soup.findAll('tr')
-    Search = re.compile(r'Q\d \d\d\d\d')
-    Search2 = re.compile(r'\$\-?\d{1,}.\d{2}')
+    Search = re.compile(r'Q\d \d\d\d\d') #Search regex for Quarters
+    Search2 = re.compile(r'\$\-?\d{1,}.\d{2}') #Search regex for EPS
     for i in range(0,len(list2)):
         mo1 = Search.search(str(list2[i])[35:42])
         b = False
@@ -49,8 +52,13 @@ for i in range(0,len(TickerList)):
             b = True
             if b == True:
                 mo2 = Search2.search(str(list2[i]))
-                dfb = next(iter(df1[df1['Quarter']== mo1.group() ].index), 'no match')
-                df1['ESP_AMZN'][dfb] = mo2.group()
+                dfb = next(iter(df1[df1['Quarter']== mo1.group() ].index), 'no match') #Searches for the matching Quarter, this is untested
+                df1['ESP_{}'.format(x)][dfb] = mo2.group() #Places it at the right place
         except:
             pass
-print(df1)
+print(df1) #With this check with the site if the last part (2005) matches
+
+#Add some code here which saves it to a .xlsx file and upload it to the drive
+#make sure it first checks if it is not already on the drive??
+#or delete previous file on the drive or something.
+#I think the code for this can be found somewhere in the CheckAndUploadDJ.py and in DriveCleanUp.py
