d46544207ca0744d26d799bb58371dc4241e451b
Dutchometrics <spa.sven@gmail.com>
2019-08-14 16:50:48 +0300

    Upload Stock data to Drive

--- ImportStockData.py
+++ ImportStockData.py
@@ -1,10 +1,53 @@
 import pandas as pd 
 import urllib.request, io, csv
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import os
 
-ticker = 'V'
-url = urllib.request.urlopen('http://download.macrotrends.net/assets/php/stock_data_export.php?t={}'.format(ticker)) 
-datareader = csv.reader(io.TextIOWrapper(url))
-df = pd.DataFrame(list(datareader)[15:], columns=['date','open','high','low','close','volume'])
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+drive = GoogleDrive(gauth)
 
-print(df.head())
+TickerList = ['MMM', #Companies you want to include in your investigation, currently Dow Jones
+'AXP',
+'AAPL',
+'BA',
+'CAT',
+'CVX',
+'CSCO',
+'KO',
+'DOW',
+'XOM',
+'GS',
+'HD',
+'IBM',
+'INTC',
+'JNJ',
+'JPM',
+'MCD',
+'MRK',
+'MSFT',
+'NKE',
+'PFE',
+'PG',
+'TRV',
+'UNH',
+'UTX',
+'VZ',
+'V',
+'WMT',
+'WBA',
+'DIS']
+
+for x in TickerList:
+    url = urllib.request.urlopen('http://download.macrotrends.net/assets/php/stock_data_export.php?t={}'.format(x)) 
+    datareader = csv.reader(io.TextIOWrapper(url))
+    df = pd.DataFrame(list(datareader)[15:], columns=['date','open','high','low','close','volume'])
+    df.to_excel('StockData_{}.xlsx'.format(x))
+    
+    file1 = drive.CreateFile()
+    file1.SetContentFile('StockData_{}.xlsx'.format(x))
+    file1.Upload()
+    print('upload {} succesvol'.format(x))
+    os.remove('StockData_{}.xlsx'.format(x))
 
