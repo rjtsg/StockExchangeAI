b7638916d015d917f54f839f8193d72621549c66
Dutchometrics <spa.sven@gmail.com>
2019-08-14 23:04:56 +0300

    merge

--- ImportStockData.py
+++ ImportStockData.py
@@ -1,10 +1,49 @@
 import pandas as pd 
 import urllib.request, io, csv
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import os
 
-ticker = 'V'
-url = urllib.request.urlopen('http://download.macrotrends.net/assets/php/stock_data_export.php?t={}'.format(ticker)) 
-datareader = csv.reader(io.TextIOWrapper(url))
-df = pd.DataFrame(list(datareader)[15:], columns=['date','open','high','low','close','volume'])
+tickers=list(pd.read_excel('tickers.xlsx').iloc[:,0])
 
-print(df.head())
 
+def getStockInfo(x): #Needs to be a string
+        url = urllib.request.urlopen('http://download.macrotrends.net/assets/php/stock_data_export.php?t={}'.format(x)) 
+        datareader = csv.reader(io.TextIOWrapper(url))
+        df = pd.DataFrame(list(datareader)[15:], columns=['date','open','high','low','close','volume'])
+        return df
+
+def saveStockInfo(TickerList): #Needs to be a list
+    for x in TickerList:
+        url = urllib.request.urlopen('http://download.macrotrends.net/assets/php/stock_data_export.php?t={}'.format(x)) 
+        datareader = csv.reader(io.TextIOWrapper(url))
+        df = pd.DataFrame(list(datareader)[15:], columns=['date','open','high','low','close','volume'])
+        df.to_excel('StockData_{}.xlsx'.format(x))
+        print(x)
+        return 'Saved to local foldern'
+
+
+
+def saveStockToDrive(TickerList): #Needs to be a list
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+
+    for x in TickerList:
+        url = urllib.request.urlopen('http://download.macrotrends.net/assets/php/stock_data_export.php?t={}'.format(x)) 
+        datareader = csv.reader(io.TextIOWrapper(url))
+        df = pd.DataFrame(list(datareader)[15:], columns=['date','open','high','low','close','volume'])
+        df.to_excel('StockData_{}.xlsx'.format(x))
+        
+        file1 = drive.CreateFile()
+        file1.SetContentFile('StockData_{}.xlsx'.format(x))
+        file1.Upload()
+        print('upload {} succesvol'.format(x))
+        os.remove('StockData_{}.xlsx'.format(x))
+    
+    return 'Saved to Google Drive'
+
+#Example how the functiones can be called for APPLE stocks
+# print(getStockInfo('AAPL'))
+# print(saveStockInfo(['AAPL']))
+# print(saveStockToDrive(['AAPL']))
