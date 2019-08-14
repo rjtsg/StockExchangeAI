670d4cd4e2e4ea33a6b338ea32712add0de887ad
Dutchometrics <spa.sven@gmail.com>
2019-08-14 21:14:05 +0300

    Import tickers from xlsx file

--- ImportStockData.py
+++ ImportStockData.py
@@ -4,50 +4,33 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
 
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
-drive = GoogleDrive(gauth)
+tickers=list(pd.read_excel('tickers.xlsx').iloc[:,0])
 
-TickerList = ['MMM', #Companies you want to include in your investigation, currently Dow Jones
-'AXP',
-'AAPL',
-'BA',
-'CAT',
-'CVX',
-'CSCO',
-'KO',
-'DOW',
-'XOM',
-'GS',
-'HD',
-'IBM',
-'INTC',
-'JNJ',
-'JPM',
-'MCD',
-'MRK',
-'MSFT',
-'NKE',
-'PFE',
-'PG',
-'TRV',
-'UNH',
-'UTX',
-'VZ',
-'V',
-'WMT',
-'WBA',
-'DIS']
 
-for x in TickerList:
-    url = urllib.request.urlopen('http://download.macrotrends.net/assets/php/stock_data_export.php?t={}'.format(x)) 
-    datareader = csv.reader(io.TextIOWrapper(url))
-    df = pd.DataFrame(list(datareader)[15:], columns=['date','open','high','low','close','volume'])
-    df.to_excel('StockData_{}.xlsx'.format(x))
+def getStockInfo(TickerList):
+    for x in TickerList:
+        url = urllib.request.urlopen('http://download.macrotrends.net/assets/php/stock_data_export.php?t={}'.format(x)) 
+        datareader = csv.reader(io.TextIOWrapper(url))
+        df = pd.DataFrame(list(datareader)[15:], columns=['date','open','high','low','close','volume'])
+    return df
+
+
+def SaveStockToDrive(TickerList):
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+
+    for x in TickerList:
+        url = urllib.request.urlopen('http://download.macrotrends.net/assets/php/stock_data_export.php?t={}'.format(x)) 
+        datareader = csv.reader(io.TextIOWrapper(url))
+        df = pd.DataFrame(list(datareader)[15:], columns=['date','open','high','low','close','volume'])
+        df.to_excel('StockData_{}.xlsx'.format(x))
+        
+        file1 = drive.CreateFile()
+        file1.SetContentFile('StockData_{}.xlsx'.format(x))
+        file1.Upload()
+        print('upload {} succesvol'.format(x))
+        os.remove('StockData_{}.xlsx'.format(x))
     
-    file1 = drive.CreateFile()
-    file1.SetContentFile('StockData_{}.xlsx'.format(x))
-    file1.Upload()
-    print('upload {} succesvol'.format(x))
-    os.remove('StockData_{}.xlsx'.format(x))
+        return 'Saved to Google Drive'
 
