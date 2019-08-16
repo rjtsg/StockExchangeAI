684536ab2b63702d17464a53787e4879cd1513f3
Dutchometrics <spa.sven@gmail.com>
2019-08-16 10:48:12 +0300

    update

--- getDividends.py
+++ getDividends.py
@@ -18,6 +18,7 @@ def getDividend(TickerList,excelfile=None):
                 df = pd.DataFrame()
         else:
                 df = pd.read_excel(excelfile)
+                print('test')
         for i in range(0,len(TickerList)):
                 x = TickerList[i]
                 url = 'https://query1.finance.yahoo.com/v8/finance/chart/{}?symbol={}&period1=0&period2=9999999999&interval=1mo&events=div'.format(x,x)
