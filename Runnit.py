5c19eec0da4d569313d3e2c712c461bdc7a53882
rjtsg <rjtsg@users.noreply.github.com>
2019-09-01 22:28:46 +0200

    Deleted some comments

--- Runnit.py
+++ Runnit.py
@@ -44,24 +44,5 @@ for x in DataList:
 os.chdir(maindirectory) #switches back to the path from which we started the run
 
 
-#PERFile = os.path.isfile('PERData.xlsx')
-#if PERFile == False:
-#    print('PE-ratios data does not exist, building a new file')
-#    df = getPER(TickerList,TickerName)
-#elif PERFile == True:
-#    PERData = pd.read_excel('PERData.xlsx',index_col=0)
-#    print('checking and updating PE-ratios')
-#    df = getPER(TickerList,TickerName,PERData)
-    
-    
-
-
-#print('getting debt/equity ratios')
-#getDER(TickerList,TickerName)
-#print('getting revenues')
-#getRev(TickerList,TickerName)
-#print('getting earnings-per-share')
-#df = getEPS(TickerList,TickerName)
-#print('succes')
 
 
