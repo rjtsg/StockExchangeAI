1c39b35baddf16850598881903e435ae300b40d9
rjtsg <rjtsg@users.noreply.github.com>
2019-09-02 22:48:45 +0200

    Updating Stock Structure

--- ImportStockData.py
+++ ImportStockData.py
@@ -4,46 +4,24 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 import os
 import yfinance as yf
+from datetime import datetime
 
-tickers=list(pd.read_excel('tickers.xlsx').iloc[:,0])
-
-
-
-def getStockInfo(x): #Needs to be a string
+def saveStockToDrive(TickerList,TickerName,path,dataframe =None): #TickerListNeeds to be a list
+    newFile=True
+    if dataframe is not None:
+        df = dataframe
+        newFile = False
+        lastDate = df.index[0]
+    for x in TickerList:
         tickr = yf.Ticker(x)
-        df = tickr.history(period="max")
-        df = df.sort_index(axis=0, ascending=False)
+        if newFile:
+                df = tickr.history(period="max")
+                df = df.sort_index(axis=0, ascending=False)
+        else:
+                today = datetime.today().strftime('%Y-%m-%d')
+                df1 = tickr.history(start=lastDate, end=today)
+                df1 = df1.sort_index(axis=0, ascending=False)
+                df1 = df1.append(df)
+                df = df1
+        df.to_excel(path+'_{}.xlsx'.format(x))
         return df
-
-def saveStockInfo(TickerList): #Needs to be a list
-    for x in TickerList:
-        tickr = yf.Ticker(x)
-        df = tickr.history(period="max")
-        df = df.sort_index(axis=0, ascending=False)
-        df.to_excel('StockData_{}.xlsx'.format(x))
-        print(x)
-        return 'Saved to local folder'
-
-def saveStockToDrive(TickerList): #Needs to be a list
-    gauth = GoogleAuth()
-    gauth.LocalWebserverAuth()
-    drive = GoogleDrive(gauth)
-
-    for x in TickerList:
-        tickr = yf.Ticker(x)
-        df = tickr.history(period="max")
-        df = df.sort_index(axis=0, ascending=False)
-        df.to_excel('StockData_{}.xlsx'.format(x))
-        
-        file1 = drive.CreateFile()
-        file1.SetContentFile('StockData_{}.xlsx'.format(x))
-        file1.Upload()
-        print('upload {} succesvol'.format(x))
-        os.remove('StockData_{}.xlsx'.format(x))
-    
-    return 'Saved to Google Drive'
-
-#Example how the functiones can be called for APPLE stocks
-#print(getStockInfo('V'))
-#print(saveStockInfo(['AAPL']))
-#print(saveStockToDrive(['V']))
