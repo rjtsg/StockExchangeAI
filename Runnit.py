bea228172bc65d76837566dcb2b31aad0dcbe547
rjtsg <rjtsg@users.noreply.github.com>
2019-09-02 23:16:42 +0200

    deleting files

--- Runnit.py
+++ Runnit.py
@@ -4,26 +4,31 @@ import xlrd
 import os
 import time
 import lxml
+import sys
 
-#from ImportPERData import getPER
-#from ImportDERData import getDER
-#from ImportRevenueData import getRev
-#from ImportEPSData import getEPS
+sys.path.append('ImportFunctions')
 
-import ImportFile
+from ImportPERData import getPER
+from ImportDERData import getDER
+from ImportEPSData import getEPS
+from ImportDividendData import getDividend
+from ImportRevenueData import getRevenue
+from ImportStockData import getStock
 
 TickerList=list(pd.read_excel('tickers.xlsx').iloc[:,0])
 TickerName=list(pd.read_excel('tickers.xlsx').iloc[:,1])
-#DataList = ['PERData.xlsx','DERData.xlsx','EPSData.xlsx','RevenueData.xlsx']
-DataList = ['PER','DER','EPS','Revenue','Dividend','Stock']
+DataList = ['PER','DER','EPS','Revenue','Dividend','Stock'] #make sure that these 2 are in the same order
+FunctionList = [getPER,getDER,getEPS,getRevenue,getDividend,getStock]
 
 if not os.path.exists('DataFiles'): #checks if directory DataFiles exists, if not makes a directory with that name
     os.makedirs('DataFiles')
     print('Created new DataFiles directory')
 
-maindirectory = os.getcwd() #remembers the path from which the file is run
+
 firsttime = True 
-for x in DataList:
+for i in range(0,len(DataList)):
+    x = DataList[i]
+    y = FunctionList[i]
     if firsttime: #such that when it is in the directory it does not keep wanting to go deeper
         firsttime = False
         os.chdir('DataFiles') #goes into the DataFiles directory
@@ -34,13 +39,13 @@ for x in DataList:
 
     if not os.path.exists(path):
         print('{} data does not exists, building a new file'.format(x))
-        df = getattr(ImportFile, 'get{}'.format(x))(TickerList,TickerName,path)
+        df = y(TickerList,TickerName,path)
     else:
         print('checking and updating {} data'.format(x))
         DataFrame = pd.read_excel('{}Data.xlsx'.format(x),index_col=0)
-        df = getattr(ImportFile, 'get{}'.format(x))(TickerList,TickerName,path,DataFrame)
-        
-        
+        df = y(TickerList,TickerName,path,DataFrame)
+
+DataDirectory = os.getcwd() #remembers the path, maybe handy for alter        
 os.chdir(maindirectory) #switches back to the path from which we started the run
 
 
