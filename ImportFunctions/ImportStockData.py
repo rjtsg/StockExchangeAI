9b8b14158e40b2720e7a07c059eaa2cc5c700e68
Sven Spa <svenspa@Svens-MacBook-Pro.local>
2019-09-07 23:01:31 +0200

    Major update for loop

--- ImportFunctions/ImportStockData.py
+++ ImportFunctions/ImportStockData.py
@@ -10,18 +10,15 @@ def getStock(TickerList,TickerName,path,dataframe =None): #TickerListNeeds to be
         df = dataframe
         newFile = False
         lastDate = df.index[0]
-    for x in TickerList:
-        tickr = yf.Ticker(x)
-        if newFile:
-                print('Stock data does not exist, building a new file')
-                df = tickr.history(period="max")
-                df = df.sort_index(axis=0, ascending=False)
-        else:
-                print('checking and updating stock data')
-                today = datetime.today().strftime('%Y-%m-%d')
-                df1 = tickr.history(start=lastDate, end=today)
-                df1 = df1.sort_index(axis=0, ascending=False)
-                df1 = df1.append(df)
-                df = df1
-        df.to_excel(path+'_{}.xlsx'.format(x))
-        return df
+    tickr = yf.Ticker(path[:-9])
+    if newFile:
+            df = tickr.history(period="max")
+            df = df.sort_index(axis=0, ascending=False)
+    else:
+            today = datetime.today().strftime('%Y-%m-%d')
+            df1 = tickr.history(start=lastDate, end=today)
+            df1 = df1.sort_index(axis=0, ascending=False)
+            df1 = df1.append(df)
+            df = df1
+    df.to_excel(path)
+    return df
