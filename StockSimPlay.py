83723bda378890d9430e3fe299638745a4315ad7
rjtsg <rjtsg@users.noreply.github.com>
2019-09-13 23:51:49 +0200

    The year is loaded, next up game representation

--- StockSimPlay.py
+++ StockSimPlay.py
@@ -15,6 +15,24 @@ Calculate annual return of trader and that of the stock. It would be nice if som
 import numpy as np 
 import pandas as pd 
 import sys 
+import os
 
 #load in the AXP data and select data from 2000 to 2001:
-sys.path.append('DataFiles')
+MainDirectory = os.getcwd()
+os.chdir('DataFiles')
+df = pd.read_excel('AXPData.xlsx')
+os.chdir(MainDirectory)
+
+#So now we only want to have the data of 2000-01-01 to 2000-12-31 roughly
+df1 = pd.DataFrame(data=None, columns=df.columns)
+counter = 0
+for i in range(len(df)):
+    datecheck = str(df.Date[i])
+    if datecheck[0:4] == '2000':
+        df1.loc[datecheck] = df.iloc[i]
+        
+#now we will have to flip it in order to make it easier for ourselfs (2000-01-01 is not the start date)
+df1 = df1.iloc[::-1]
+#we apperently start with 2000-01-03, which is probably due to missing data.
+#So from here on we will have to build some while loop that goes on untill the end of the data is reached
+#It must containt buying and selling opertunities as described above!
